23c4f324876090581dd3adc07ea42e5eadd9cd1c
kartik chhabra <chhabrakartik6@gmail.com>
2023-11-23 15:02:05 +0530

    🔨 chore : update footer component

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,34 +1,18 @@
 import React from "react";
-import styled from "styled-components";
-
-const FooterContainer = styled.footer`
-  background-color: #f2f2f2;
-  padding: 14px;
-  text-align: center;
-`;
-
-const FooterText = styled.p`
-  font-size: 14px;
-  font-weight: 700;
-  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-  color: #333;
-`;
-
-const DeveloperName = styled.span`
-  font-weight: bold;
-  color: #ff4081;
-`;
-
-
+import "./footer.css";
 const Footer = () => {
   const developerName = "Kartik Chhabra";
   const CurrentYear = new Date().getFullYear();
   return (
-    <FooterContainer>
-      <FooterText>
-        Made with ❤️ by <DeveloperName>{developerName} | {CurrentYear}</DeveloperName>
-      </FooterText>
-    </FooterContainer>
+    <footer>
+      <p>
+        Made with ❤️ by{" "}
+        <a href="https://in.linkedin.com/in/kartik-chhabra-65a2b822b" rel="">
+          {developerName}
+        </a>{" "}
+        | {CurrentYear}
+      </p>
+    </footer>
   );
 };
 
